1357fafd3879014d1f9926cccddd9ba4bcd63cee
rittels <44720431+rittels@users.noreply.github.com>
2021-01-16 22:29:13 +0200

    Add files via upload

---
+++ examples/11-mobile-demo/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	displayName: 'drag demo',
+	hook: 'contextmenu',
+   	className: 'plugin-lhpane plugin-mobile-fullscreen-no-header',
+
+    //classNameMobile:
+
+    //You can use the default mobile class:
+    //'plugin-mobile-fullscreen'
+
+    //or create your own:
+    //'plugin-dragdemo-mobile'
+    //but no longer necessary,  just use built in class:   plugin-mobile-fullscreen-no-header.
+
+
+    //dependencies: [],
+    exclusive: 'lhpane',
+}
