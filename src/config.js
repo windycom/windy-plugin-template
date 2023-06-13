e0738cbb7257ce9259e773319d23a4f6f6622bcd
Jakub Vrána <jakub@vrana.cz>
2023-06-13 11:56:21 +0200

    Update src to v39

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
-module.exports = {
+export default {
     // Display name of your plugin, as it will appear in the menu
     displayName: 'My super plugin',
 
-    // Place, where opening link to your plugin will appear. So far only
+    // Place where opening link to your plugin will appear
     // Allowed: 'contextmenu', 'menu'
     hook: 'menu',
 
-    // List of external libraries, that should be loaded before
+    // List of external libraries that should be loaded before
     // your plugin is even mounted to the page
-    // dependencies: ['https://unpkg.com/d3@5.7.0/dist/d3.min.js']
+    // dependencies: ['https://unpkg.com/d3@5.7.0/dist/d3.min.js'],
 
     // List of classnames that will be attached to your plugin upon mounting
     // className: 'plugin-lhpane plugin-mobile-fullscreen',
@@ -20,11 +20,11 @@ module.exports = {
     // Allowed: 'lhpane', 'rhpane' and 'all'
     // exclusive: 'lhpane',
 
-    // The place in page, where your plugin element will be mounted
+    // The place in page where your plugin element will be mounted
     // to the page. By default all the plugins are attached to
     // #plugins div
     // attachPoint: '#map_container .leaflet-popup-pane',
 
     // If you want to apply different mounting point on mobile devices
-    // attachPointMobile: '#plugins'
+    // attachPointMobile: '#plugins',
 };
---
+++ src/plugin.js
@@ -0,0 +1,5 @@
+console.log('I am mounted to the page');
+
+export const onopen = () => console.log('I am opened');
+
+export const onclose = () => console.log('I am being closed');
